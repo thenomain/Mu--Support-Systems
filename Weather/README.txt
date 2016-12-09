eded0c442e5d6d4408dea94b577254dc2e6d613d
Kent Jenkins <thenomain@gmail.com>
2016-12-09 16:59:08 -0500

    new: Almost Real-Time Weather System

---
+++ Weather/README.txt
@@ -0,0 +1,91 @@
+The "Hourly Weather Conditions" system, originally by Brus (I'm pretty sure), 
+modified when Yahoo changed their API system to now use Weather Underground.
+
+You will need to have access to the shell and to cron on the shell.
+
+
+** STEP ONE: Get an API code ***************************************************
+
+Go to this site:
+	https://www.wunderground.com/weather/api/d/pricing.html
+
+Sign up for a free Anvil Plan (for developers). This will be used via a php 
+fetching system, so the closest choice of what this will be used for is "web".
+
+You will get a code in the mail that will run about 10 games or locations on one 
+code. Don't share this code.
+
+
+** STEP TWO: Work out your location ********************************************
+
+Wunderground is pretty good about turning city names into locations, so you'll 
+need the state and city (if U.S.A., otherwise I'm not currently sure).
+
+Let's say your API code is "b9bfg15x30ya3a0x", your state is "Vermont" and your 
+city is "Stowe". Test that you're getting the weather that you need:
+
+	http://api.wunderground.com/api/b9bfg15x30ya3a0x/conditions/forecast/
+	alert/astronomy/q/VT/Stowe.json
+
+
+** STEP THREE: Edit and upload weather.php *************************************
+
+At the start of the "weather.php" file will be a few things you'll need to change. 
+
+
+** STEP FOUR: Create weather_api_token.txt *************************************
+
+In the same directory as weather.php, type the following, replacing 
+xxxxxxxxxxxxx with the API code mailed to you by Weather Underground:
+
+cat > weather_api_token.txt
+<?php 
+$api_token = "xxxxxxxxxxxxx";
+?>
+ctrl-d
+
+
+** STEP FIVE: Run the code 
+
+Still logged into the server, type:
+
+php weather.php
+
+This runs the very first weather-grabbing from Weather Underground. Hopefully 
+there were no errors.
+
+
+** STEP SIX: Set the Cron ******************************************************
+
+You need to run that command once an hour. If you already know how to add to the 
+crontab, add this line:
+
+	@hourly cd <exact path to where 'weather.php' lives>; php weather.php > /dev/null 2>&1
+
+Some servers don't let you edit this from the command line, and you'll have to 
+find where to add to the cron in the CPANEL. Enter the above except for the word 
+"@hourly", which should be an option similar to "do this once an hour".
+
+
+** STEP SEVEN: Check the game and load the softcode ****************************
+
+On the game as a wizard, type "@readcache", then type "meteo". If everything has 
+run properly up until now, you will see the main help file for meteorology.
+"meteo conditions" for the current conditions, and so forth.
+
+If all this is good, upload the softcode system in "weather.txt" and test it by 
+typing "weather".
+
+
+** STEP EIGHT: Start the Mushcron **********************************************
+
+The game needs to run "@readcache" once an hour. In your Myrddin's mushcron system, add the following lines:
+
+&CRON_TIME_WEATHER mushcron=||||01 02|
+&CRON_JOB_WEATHER mushcron=@readcache
+
+
+** STEP NINE: Done! ************************************************************
+
+Check that by 2 minutes after the hour, the weather has been updated. If it has, 
+then you're done. If not, ask your local psychocoder for assistance.
---
+++ Weather/weather.php
@@ -0,0 +1,353 @@
+<?php
+/******************************************************************************
+ *
+ * Sign up for your free Anvil Plan (for Developers) here:
+ *    https://www.wunderground.com/weather/api/d/pricing.html
+ * 
+ * Then enter the code into "weather_api_token.txt" as:
+ *    <?php 
+ *    $api_token = "xxxxxxxxxxxxx";
+ *    ?>
+ * 
+ * You have limited lookups per hour and day. Do not share this key.
+ * 
+ * Remember to: 
+ *    chmod o-r weather_api_token.txt 
+ * 
+ * Make sure it runs hourly. Add the following to your crontab:
+ *    @hourly cd <exact path to where 'weather.php' lives>; 
+ *    php weather.php > /dev/null 2>&1
+ * 
+ * Read under "User configuration section" for what to add to <game>.conf
+ *
+ *****************************************************************************/
+
+require "weather_api_token.txt";
+
+/******************************************************************************
+ *
+ * User configuration section. Please read included comments.
+ *
+ *****************************************************************************/
+
+// the state and city to pull 
+
+$state = "MD"; 
+$city = "Upper_Marlboro"; 
+
+/*
+   Where to put the final file; use exact path.
+   This file name must be in the netmux.conf, for example: 
+	   helpfile meteo  /home/bitn/game/etc/text/weather
+*/
+
+$file = '/home/bitn/game/etc/text/weather.txt'; 
+
+/******************************************************************************
+ *
+ * No configuration is required beyond this point.
+ * 
+ * Basic setup and file reading.
+ *
+ *****************************************************************************/
+
+$base_string = 'http://api.wunderground.com/api/' . $api_token . 
+	'/conditions/forecast/alert/astronomy/q/' . $state . '/' . $city . '.json';
+$json_string = file_get_contents( $base_string ); 
+$weather = json_decode( $json_string ); 
+
+@$error = $weather->response->error->description; 
+if( isset( $error )) { 
+	$file_error = "& help\nError from query: " . $error . "\n\n";
+	$file_error .= "Last Updated on " . date( 'F d, h:i A T' ) . "\n";
+	$fr = fopen( $file, 'w' );
+	fputs( $fr, $file_error );
+	fclose( $fr );
+	die( $error ); 
+}
+
+/******************************************************************************
+ * 
+ * Conditions (current observations) : $conditions
+ * 
+ * [$weather->current_observation]
+ * temperature: ->temperature_string ["83.8 F (28.8 C)"]
+ * feels like: ->feelslike_string ["81 F (27 C)"]
+ * humidity: ->relative_humidity ["22%"]
+ * 
+ * wind description: ->wind_string ["Calm"]
+ * wind direction: ->wind_dir ["East"]
+ * wind speed: ->wind_mph & ->wind_kph ["0.0" & "0"]
+ * wind gust: ->wind_gust_mph & ->wind_gust_kph ["3.0" & "4.8"]
+ * 
+ * pressure (inches): ->pressure_in ["30.15"]
+ * pressure trend: ->pressure_trend ["-"]
+ * 
+ * visibility: ->visibility_mi & ->visibility_km ["10.0" & "16.1"]
+ * 
+ ******************************************************************************/
+
+$conditions = $weather->current_observation; 
+switch( $conditions->pressure_trend ) { 
+	case "-": $pressure_trend = "Falling"; break; 
+	case "+": $pressure_trend = "Rising"; break; 
+	default: $pressure_trend = "Steady"; break; 
+}
+
+$file_conditions = "& conditions\n"; 
+$file_conditions .= "Conditions: " .  $conditions->weather . "\n"; 
+$file_conditions .= "Temperature: " . 
+	$conditions->temperature_string . "\n"; 
+if ( $conditions->temperature_string != $conditions->feelslike_string ) { 
+	$file_conditions .= "Feels Like: " . 
+		$conditions->feelslike_string . "\n";
+}
+$file_conditions .= "Humidity: " . 
+	$conditions->relative_humidity . "% \n"; 
+$file_conditions .= "Wind: " . 
+	$conditions->wind_mph . " mph " . 
+	$conditions->wind_dir . " (" . 
+	$conditions->wind_kph . " kph)\n"; 
+$file_conditions .= "Wind String: " . 
+	$conditions->wind_string . "\n";
+if ( $conditions->wind_gust_mph != 0 ) { 
+	$file_conditions .= "Wind Gusts: " . 
+		$conditions->wind_gust_mph . " mph (" . 
+		$conditions->wind_gust_kph . " kph)\n"; 
+}
+$file_conditions .= "Pressure: " . 
+	$conditions->pressure_in . "\" and " . 
+	$pressure_trend . "\n"; 
+$file_conditions .= "Visibility: " . 
+	$conditions->visibility_mi . " mi (" . 
+	$conditions->visibility_km . " km)\n"; 
+if ( $conditions->precip_1hr_in != 0 ) { 
+	$file_conditions .= "Hour Precip: " . 
+		$conditions->precip_1hr_string . "\n";
+}
+if ( $conditions->precip_today_in != 0 ) { 
+	$file_conditions .= "Day Precip: " . 
+		$conditions->precip_today_string . "\n";
+}
+
+// "precip_1hr_string":"0.00 in ( 0 mm)"
+
+/******************************************************************************
+ * 
+ * Forecast (today & tomorrow) : $forecast
+ * 
+ * high: ->high->fahrenheit & ->high->celsius
+ * low: ->low->fahrenheit & ->low->celsius
+ * condition: ->conditions
+ * average wind: ->avewind->mph & ->avewind->kph & ->avewind->string (direction)
+ * max wind: ->maxwind->mph & ->maxwind->kph & ->maxwind->string (direction)
+ * average humidity: ->avehumidity
+ * 
+ * [Quantitative Precipitation Forecasts: melted liquid impact on the area]
+ * predicted rainfall: ->qpf_allday->in & ->qpf_allday->mm
+ * rainfall for day: ->qpf_day->in & ->qpf_day->mm
+ * rainfall for night: ->qpf_night->in & ->qpf_day->mm
+ * predicted snowfall: ->snow_allday->in & ->snow_allday->cm
+ * snowfall for day: ->snow_day->in & ->snow_day->cm
+ * snowfall for night: ->snow_night->in & ->snow_night->cm
+ * 
+ ******************************************************************************/
+
+$today = $weather->forecast->simpleforecast->forecastday[0];
+$tomorrow = $weather->forecast->simpleforecast->forecastday[1];
+
+// today's forecast 
+
+$file_today = "& today\n"; 
+$file_today .= "High: " . 
+	$today->high->fahrenheit . " F (" . 
+	$today->high->celsius ." C)\n"; 
+$file_today .= "Low: " . 
+	$today->low->fahrenheit . " F (" . 
+	$today->low->celsius ." C)\n"; 
+$file_today .= "Conditions: " . 
+	$today->conditions . "\n";  
+$file_today .= "Wind: " . 
+	$today->avewind->mph . " mph " . 
+	$today->avewind->dir . " (" . 
+	$today->avewind->kph . " kph)\n"; 
+if ( $today->maxwind->mph != 0 ) { 
+$file_today .= "Wind Gusts: " . 
+	$today->maxwind->mph . " mph " . 
+	$today->maxwind->dir . " (" . 
+	$today->maxwind->kph . " kph)\n"; 
+}
+$file_today .= "Average Humidity: " . 
+	$today->avehumidity . "\% \n"; 
+$file_today .= "Predicted Precipitation: " . 
+	$today->qpf_allday->in . " in (" . 
+	$today->qpf_allday->mm . " mm)\n"; 
+if ( $today->qpf_allday->in != 0 ) { 
+	$file_today .= "Precipitation (Day): " . 
+		$today->qpf_day->in . " in (" . 
+		$today->qpf_day->mm . " mm)\n"; 
+	$file_today .= "Precipitation (Night): " . 
+		$today->qpf_night->in . " in (" . 
+		$today->qpf_night->mm . " mm)\n"; 
+}
+if ( $today->snow_allday->in != 0 ) { 
+	$file_today .= "Predicted Snowfall: " . 
+		$today->snow_allday->in . " in (" . 
+		$today->snow_allday->cm . " cm)\n"; 
+	$file_today .= "Snowfall (Day): " . 
+		$today->snow_day->in . " in (" . 
+		$today->snow_day->cm . " cm)\n"; 
+	$file_today .= "Snowfall (Night): " . 
+		$today->snow_night->in . " in (" . 
+		$today->snow_night->cm . " cm)\n"; 
+}
+
+// tomorrow's forecast
+
+$file_tomorrow = "& tomorrow\n"; 
+$file_tomorrow .= "High: " . 
+	$tomorrow->high->fahrenheit . " F (" . 
+	$tomorrow->high->celsius ." C)\n"; 
+$file_tomorrow .= "Low: " . 
+	$tomorrow->low->fahrenheit . " F (" . 
+	$tomorrow->low->celsius ." C)\n"; 
+$file_tomorrow .= "Conditions: " . 
+	$tomorrow->conditions . "\n";  
+$file_tomorrow .= "Wind: " . 
+	$tomorrow->avewind->mph . " mph " . 
+	$tomorrow->avewind->dir . " (" . 
+	$tomorrow->avewind->kph . " kph)\n"; 
+if ( $tomorrow->maxwind->mph != 0 ) { 
+$file_tomorrow .= "Wind Gusts: " . 
+	$tomorrow->maxwind->mph . " mph " . 
+	$tomorrow->maxwind->dir . " (" . 
+	$tomorrow->maxwind->kph . " kph)\n"; 
+}
+$file_tomorrow .= "Average Humidity: " . 
+	$tomorrow->avehumidity . "\% \n"; 
+$file_tomorrow .= "Predicted Precipitation: " . 
+	$tomorrow->qpf_allday->in . " in (" . 
+	$tomorrow->qpf_allday->mm . " mm)\n"; 
+if ( $tomorrow->qpf_allday->in != 0 ) { 
+	$file_tomorrow .= "Precipitation (Day): " . 
+		$tomorrow->qpf_day->in . " in (" . 
+		$tomorrow->qpf_day->mm . " mm)\n"; 
+	$file_tomorrow .= "Precipitation (Night): " . 
+		$tomorrow->qpf_night->in . " in (" . 
+		$tomorrow->qpf_night->mm . " mm)\n"; 
+}
+if ( $tomorrow->snow_allday->in != 0 ) { 
+	$file_tomorrow .= "Predicted Snowfall: " . 
+		$tomorrow->snow_allday->in . " in (" . 
+		$tomorrow->snow_allday->cm . " cm)\n"; 
+	$file_tomorrow .= "Snowfall (Day): " . 
+		$tomorrow->snow_day->in . " in (" . 
+		$tomorrow->snow_day->cm . " cm)\n"; 
+	$file_tomorrow .= "Snowfall (Night): " . 
+		$tomorrow->snow_night->in . " in (" . 
+		$tomorrow->snow_night->cm . " cm)\n"; 
+}
+
+/******************************************************************************
+ * 
+ * Alerts (emergency information) : $alerts
+ * 
+ * ->alerts->description
+ * ->alerts->expires
+ * ->alerts->message (\u000A -> %r)
+ * 
+ ******************************************************************************/
+
+@$alerts = $weather->alerts; 
+@$error = $alerts->description; 
+
+$file_alerts = "& alerts\n"; 
+
+if( isset( $error )) { 
+	$file_alerts .= "Description: " . $alerts->description . "\n";
+	$file_alerts .= "Expires: " . $alerts->expires . "\n";
+	$file_alerts .= "Message: " . $alerts->message . "\n";
+}
+else { 
+	$file_alerts .= "No alerts in your area.\n"; 
+};
+
+
+/******************************************************************************
+ * 
+ * Astronomy (sunrise, sunset, moonrise, moonset, etc. ) : $astronomy
+ * 
+ * ->sun_phase->sunrise->hour & ->sun_phase->sunrise->minute 
+ * ->sun_phase->sunset->hour & ->sun_phase->sunset->minute 
+ * 
+ * ->moon_phase->percentIlluminated
+ * ->moon_phase->phaseofMoon
+ * ->moon_phase->hemisphere
+ * ->moon_phase->moonrise->hour & ->moon_phase->moonrise->minute
+ * ->moon_phase->moonset->hour & ->moon_phase->moonset->minute
+ * 
+ ******************************************************************************/
+
+$sun = $weather->sun_phase; 
+$moon = $weather->moon_phase; 
+
+$file_astronomy = "& astronomy\n";
+$file_astronomy .= "Sunrise: " . 
+	$sun->sunrise->hour . ":" . 
+	$sun->sunrise->minute . "\n";
+$file_astronomy .= "Sunset: " . 
+	$sun->sunset->hour . ":" . 
+	$sun->sunset->minute . "\n";
+if( isset( $moon->moonrise->hour )) { 
+	$file_astronomy .= "Moonrise: " . 
+		$moon->moonrise->hour . ":" . 
+		$moon->moonrise->minute . "\n";
+}
+if( isset( $moon->moonset->hour )) { 
+	$file_astronomy .= "Moonset: " . 
+		$moon->moonset->hour . ":" . 
+		$moon->moonset->minute . "\n";
+}
+$file_astronomy .= "Moon Phase: " . 
+	$moon->phaseofMoon . " (" . 
+	$moon->percentIlluminated . "\%)\n";
+
+
+/******************************************************************************
+ * 
+ * Top level of the 'meteo' help file
+ * 
+ ******************************************************************************/
+
+$file_help = "& help
+Raw weather data for weather code.
+
+Conditions: Current conditions as of the Last Updated time.
+Today: Today's forecast.
+Tomorrow: Tomorrow's forecast.
+Astronomy: Sun & Moon facts.
+Alerts: Emergency weather service alerts.
+Credits: People and things to thank.
+
+" . $weather->current_observation->observation_time . "\n"; // Last Updated on ...
+
+$file_credits = "& credits
+Data provided by the Weather Underground under the following terms of service:
+    " . $weather->response->termsofService . "
+
+PHP and TinyMUX code originally by Brus using the Yahoo Weather API.
+Rewritten by Thenomain using the Weather Underground API.\n";
+
+// write everything to the file
+
+$fr = fopen( $file, 'w' );
+fputs( $fr, $file_help );
+fputs( $fr, $file_conditions );
+fputs( $fr, $file_today );
+fputs( $fr, $file_tomorrow );
+fputs( $fr, $file_astronomy );
+fputs( $fr, $file_alerts );
+fputs( $fr, $file_credits );
+fclose( $fr );
+
+?>
